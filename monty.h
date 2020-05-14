6c5b0c40db4bdb894f667f70c7cda74aa2a77fc9
Ang3l1t0 <1546@holbertonschool.com>
2020-05-14 09:45:03 -0500

    add unused prototype

--- monty.h
+++ monty.h
@@ -23,7 +23,7 @@ void _pall(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
-void _nop(stack_t **stack, unsigned int line_number);
+void _nop(UNUSED stack_t **stack, UNUSED unsigned int line_number);
 
 /* Error Messages */
 void file_error(void);
